75f40ee04c09b0e43afed6fe355f9b8934009de6
Axel Díaz <diaz.axelio@gmail.com>
2014-10-09 11:31:06 -0430

    Mejorando paso de parametros

--- install_fengoffice.sh
+++ install_fengoffice.sh
@@ -10,45 +10,51 @@ REPOSITORIO='http://sourceforge.net/projects/opengoo/files/fengoffice'
 MYSQL=`which mysql`
 
 EXITO "Iniciando instalación de paquetes necesarios. Ingrese contraseña del Super Usuario"
-su -c "aptitude install apache2 php5 php5-gd php5-mysql mysql-server; a2enmod php5; invoke-rc.d apache2 restart"
+#su -c "aptitude install apache2 php5 php5-gd php5-mysql mysql-server; a2enmod php5; invoke-rc.d apache2 restart"
+
+echo "Introduzca el directorio de apache donde se encuentre el Feng Office que desee actualizar. Ejemplo1: fengoffice. Ejemplo2: fo_prueba."
+read DIR_FENG
 
 EXITO "A continuación se crearán la bd y el usuario que utilizará Feng Office"
 echo "Por favor, ingrese el usuario de la base de datos"
 read USERDB
 
 echo "Por favor, ingrese el nombre de la base de datos"
-read DB
+read NAMEDB
 
 echo "Por favor, ingrese la contraseña de la base de datos"
 read PASSDB
 
 Q0="CREATE USER '$USERDB'@'localhost' IDENTIFIED BY '$PASSDB';"
-Q1="CREATE DATABASE IF NOT EXISTS $DB;"
-Q2="GRANT ALL ON $DB.* TO '$USERDB'@'localhost' IDENTIFIED BY '$PASSDB';"
+Q1="CREATE DATABASE IF NOT EXISTS $NAMEDB;"
+Q2="GRANT ALL ON $NAMEDB.* TO '$USERDB'@'localhost' IDENTIFIED BY '$PASSDB';"
 Q3="FLUSH PRIVILEGES;"
 SQL="${Q0}${Q1}${Q2}${Q3}"
 
 EXITO "Por favor, introduzca la contraseña root de mysql"
-MYSQL -uroot -p -e "$SQL"
+$MYSQL -uroot -p -e "$Q0"
+EXITO "Por favor, introduzca la contraseña root de mysql"
+$MYSQL -uroot -p -e "$SQL"
 
 echo "Introduzca la versión a la que quiere instalar. La última probada en este escript fue la 2.7.1.1"
 read VERSION
 EXITO "Ha introducido la version $VERSION"
 sleep 1
 
-mkdir -p /tmp/fengoffice/
-cd /tmp/fengoffice/
+mkdir -p /tmp/$DIR_FENG/
+cd /tmp/$DIR_FENG/
 
 EXITO "Descargando la versión $VERSION"
-wget -c $REPOSITORIO/fengoffice_$VERSION/fengoffice_$VERSION.zip
+#wget -c $REPOSITORIO/fengoffice_$VERSION/fengoffice_$VERSION.zip
 
 unzip fengoffice_$VERSION.zip
 rm fengoffice_$VERSION.zip
-rsync -azvhP /tmp/fengoffice $DIR_APACHE
-cd $DIR_APACHE/fengoffice
+rsync -azvhP /tmp/$DIR_FENG $DIR_APACHE
 
 EXITO "Otorgando permisos a cache/ config/ tmp/ upload/"
 sleep 1
-cd $DIR_APACHE/fengoffice/
-chmod -R o+w cache/ config/ tmp/ upload/
+chmod -R 777 $DIR_APACHE/$DIR_FENG/cache/
+chmod -R 777 $DIR_APACHE/$DIR_FENG/config/
+chmod -R 777 $DIR_APACHE/$DIR_FENG/tmp/
+chmod -R 777 $DIR_APACHE/$DIR_FENG/upload/
 EXITO "Instalación culminada. Por favor, ingrese a http://localhost/fengoffice/ para continuar la instalación vía web."
